07184402e7021ffa3acdfab4079ca992bd863dc5
beasleyDotcom <beasley.com@gmail.com>
2020-07-10 14:23:11 -0700

    I am your server!

--- server.js
+++ server.js
@@ -1,38 +1,76 @@
 'use strict';
-//"require" brings in a library. this one is called express. Then saves it as express.
-const express = require('express')
 
-//bring in the dotenv library
-//the job of this library is to find hte .env file and get the variable out of it so we can use them in our js file.
+// this will bring in my express libraray
+const express = require('express');
+
+// bring in the dotenv library
+// the job of this library is to find the .env file and get the variables out of it so we can use them in our JS file
 require('dotenv').config();
 
+// this gives us a variable that we can use to run all the methods that are in the express library
+const app = express();
 
-//this gives us a variable that we can use to run all the methods that are in the express library.
+// this lets us serve a website from a directory
+app.use(express.static('./public'));
 
-const app = express()
-//the dotenv library lets us grab the POT var from the.env using the magic workds process.env.variableName
-
+// the dotenv library lets us grab the PORT var from the .env using the magic words process.env.variableName
 const PORT = process.env.PORT;
-app.use(express.status('./public'));
+ 
+// app.get('/', function (request, response) {
+//   response.send('Hello World');
+// });
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
-
-
-
-app.get('/bananas', (request, response)=> {
-    response.send('Hello World and I"m totally bananas')
-  })
-
-//this turns on the server
-//black moves in to the house located at port 3000.
-//three ways to turn on a server 
-// one is: node server.js in termninal
-//npm start
-//this is the best way:
-//nodemon
+// app.get('/bananas', (request, response) => {
+//   response.send('I am bananas about bananas');
+// });
+ 
+// turn on the server
 app.listen(PORT, () => {
-    console.log(`listening on the ${PORT}`)
+  console.log(`listening on ${PORT}`);
 });
 
+
+
+
+
+
+
+// 'use strict';
+// //"require" brings in a library. this one is called express. Then saves it as express.
+// const express = require('express')
+
+// //bring in the dotenv library
+// //the job of this library is to find hte .env file and get the variable out of it so we can use them in our js file.
+// require('dotenv').config();
+
+
+// //this gives us a variable that we can use to run all the methods that are in the express library.
+
+// const app = express()
+// //the dotenv library lets us grab the POT var from the.env using the magic workds process.env.variableName
+
+// const PORT = process.env.PORT;
+// app.use(express.status('./public'));
+
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
+
+
+
+// app.get('/bananas', (request, response)=> {
+//     response.send('Hello World and I"m totally bananas')
+//   })
+
+// //this turns on the server
+// //black moves in to the house located at port 3000.
+// //three ways to turn on a server 
+// // one is: node server.js in termninal
+// //npm start
+// //this is the best way:
+// //nodemon
+// app.listen(PORT, () => {
+//     console.log(`listening on the ${PORT}`)
+// });
+
+// //git pull upstream master
